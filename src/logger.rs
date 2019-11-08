45b28fcef8c6c55c882f563656a18cc32fc0ca9f
ndelvalle <nicolas.delvalle@gmail.com>
2019-11-08 01:29:10 -0300

    Update logger implementation

--- src/logger.rs
+++ src/logger.rs
@@ -2,12 +2,10 @@ use crate::failure::Error;
 use slog::{Drain, Level, LevelFilter};
 use std::str::FromStr;
 
-pub struct Logger {
-    pub instance: slog::Logger,
-}
+pub struct Logger {}
 
 impl Logger {
-    pub fn new(level: &str) -> Result<Self, Error> {
+    pub fn new(level: &str) -> Result<slog::Logger, Error> {
         let logger_level = Level::from_str(level).unwrap();
         let decorator = slog_term::TermDecorator::new().build();
 
@@ -18,35 +16,6 @@ impl Logger {
         let app_name = env!("CARGO_PKG_NAME");
         let app_version = env!("CARGO_PKG_VERSION");
 
-        let logger = Self {
-            instance: slog::Logger::root(drain, o!(app_name => app_version)),
-        };
-
-        Ok(logger)
-    }
-
-    pub fn child(&self, context: String) -> Self {
-        let instance = self.instance.new(o!("context" => context));
-        Self { instance }
-    }
-
-    pub fn critical(&self, message: &str) {
-        crit!(self.instance, "{}", message);
-    }
-
-    pub fn error(&self, message: &str) {
-        error!(self.instance, "{}", message);
-    }
-
-    pub fn warn(&self, message: &str) {
-        warn!(self.instance, "{}", message);
-    }
-
-    pub fn info(&self, message: &str) {
-        info!(self.instance, "{}", message);
-    }
-
-    pub fn debug(&self, message: &str) {
-        debug!(self.instance, "{}", message);
+        Ok(slog::Logger::root(drain, o!(app_name => app_version)))
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -4,19 +4,18 @@ use crate::rocket::response::content;
 use crate::rocket::Rocket;
 use crate::rocket::State;
 
-use crate::logger::Logger;
 use crate::settings::Settings;
 
 pub struct Server {}
 
 impl Server {
-    pub fn new(settings: &Settings, logger: Logger) -> Result<Rocket, Error> {
-        logger.debug("Setting up server environment");
+    pub fn new(settings: &Settings, logger: slog::Logger) -> Result<Rocket, Error> {
+        debug!(logger, "Setting up server environment");
 
         let environment = settings.environment.clone();
         let rocket_env = environment.parse::<Environment>().unwrap();
 
-        logger.debug("Setting up server address and port");
+        debug!(logger, "Setting up server address and port");
         let config = Config::build(rocket_env)
             .address(&settings.server.address)
             .port(settings.server.port)
@@ -24,9 +23,9 @@ impl Server {
             .finalize()
             .unwrap();
 
-        logger.debug("Setting up server routes and middlewares");
+        debug!(logger, "Setting up server routes and middlewares");
         let server = rocket::custom(config)
-            .manage(logger.child("router".to_string()))
+            .manage(logger.new(o!("context" => "router")))
             .mount("/", routes![index]);
 
         Ok(server)
@@ -34,8 +33,8 @@ impl Server {
 }
 
 #[get("/")]
-fn index(logger: State<Logger>) -> content::Json<&'static str> {
-    logger.debug("Retrieving hello world route status");
+fn index(logger: State<slog::Logger>) -> content::Json<&'static str> {
+    debug!(logger, "Retrieving hello world route status");
 
     content::Json(r#"{ "hello": "world" }"#)
 }
