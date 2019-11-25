d209194068730df9f9a7c4c28a6728cf754e2770
ndelvalle <nicolas.delvalle@gmail.com>
2019-11-24 21:53:51 -0300

    Format code

--- src/components/posts/model.rs
+++ src/components/posts/model.rs
@@ -7,67 +7,67 @@ use super::schema::posts::dsl::posts as collection;
 
 #[derive(Serialize, Queryable, Debug, Clone)]
 pub struct Post {
-  pub id: i32,
-  pub title: String,
-  pub body: String,
-  pub published: bool,
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
 }
 
 #[derive(Serialize, Deserialize, Insertable)]
 #[table_name = "posts"]
 pub struct NewPost {
-  pub title: String,
-  pub body: String,
-  pub published: bool,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
 }
 
 impl Post {
-  pub fn find(conn: &PgConnection) -> Vec<Post> {
-    collection
-      .order(posts::id.desc())
-      .load::<Post>(&*conn)
-      .expect("error loading posts")
-  }
+    pub fn find(conn: &PgConnection) -> Vec<Post> {
+        collection
+            .order(posts::id.desc())
+            .load::<Post>(&*conn)
+            .expect("error loading posts")
+    }
 
-  pub fn create(post: NewPost, conn: &PgConnection) -> bool {
-    diesel::insert_into(posts::table)
-      .values(&post)
-      .execute(conn)
-      .is_ok()
-  }
+    pub fn create(post: NewPost, conn: &PgConnection) -> bool {
+        diesel::insert_into(posts::table)
+            .values(&post)
+            .execute(conn)
+            .is_ok()
+    }
 
-  pub fn find_by_id(id: i32, conn: &PgConnection) -> Vec<Post> {
-    collection
-      .find(id)
-      .load::<Post>(conn)
-      .expect("Error loading post")
-  }
+    pub fn find_by_id(id: i32, conn: &PgConnection) -> Vec<Post> {
+        collection
+            .find(id)
+            .load::<Post>(conn)
+            .expect("Error loading post")
+    }
 
-  pub fn update(id: i32, conn: &PgConnection, post: NewPost) -> bool {
-    use super::schema::posts::dsl::{body as b, published as p, title as t};
-    let NewPost {
-      title,
-      body,
-      published,
-    } = post;
+    pub fn update(id: i32, conn: &PgConnection, post: NewPost) -> bool {
+        use super::schema::posts::dsl::{body as b, published as p, title as t};
+        let NewPost {
+            title,
+            body,
+            published,
+        } = post;
 
-    diesel::update(collection.find(id))
-      .set((b.eq(body), p.eq(published), t.eq(title)))
-      .get_result::<Post>(conn)
-      .is_ok()
-  }
+        diesel::update(collection.find(id))
+            .set((b.eq(body), p.eq(published), t.eq(title)))
+            .get_result::<Post>(conn)
+            .is_ok()
+    }
 
-  pub fn delete(id: i32, conn: &PgConnection) -> bool {
-    if Post::find_by_id(id, conn).is_empty() {
-      return false;
-    };
-    diesel::delete(collection.find(id)).execute(conn).is_ok()
-  }
+    pub fn delete(id: i32, conn: &PgConnection) -> bool {
+        if Post::find_by_id(id, conn).is_empty() {
+            return false;
+        };
+        diesel::delete(collection.find(id)).execute(conn).is_ok()
+    }
 
-  pub fn find_by_title(title: String, conn: &PgConnection) -> Vec<Post> {
-    collection
-      .filter(posts::title.eq(title))
-      .load::<Post>(conn)
-      .expect("Error loading posts by title")
-  }
+    pub fn find_by_title(title: String, conn: &PgConnection) -> Vec<Post> {
+        collection
+            .filter(posts::title.eq(title))
+            .load::<Post>(conn)
+            .expect("Error loading posts by title")
+    }
 }
--- src/database.rs
+++ src/database.rs
@@ -13,42 +13,42 @@ use crate::settings::Settings;
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 pub struct Database {
-  pub pool: Pool,
+    pub pool: Pool,
 }
 
 impl Database {
-  pub fn new(settings: &Settings, logger: slog::Logger) -> Result<Self, Error> {
-    let url = &settings.database.url;
-    debug!(logger, "Setting up database using {} url", url);
+    pub fn new(settings: &Settings, logger: slog::Logger) -> Result<Self, Error> {
+        let url = &settings.database.url;
+        debug!(logger, "Setting up database using {} url", url);
 
-    let manager = ConnectionManager::<PgConnection>::new(url);
-    let this = Self {
-      pool: r2d2::Pool::new(manager)?,
-    };
+        let manager = ConnectionManager::<PgConnection>::new(url);
+        let this = Self {
+            pool: r2d2::Pool::new(manager)?,
+        };
 
-    Ok(this)
-  }
+        Ok(this)
+    }
 }
 
 pub struct Connection(pub r2d2::PooledConnection<ConnectionManager<PgConnection>>);
 
 impl<'a, 'r> FromRequest<'a, 'r> for Connection {
-  type Error = ();
+    type Error = ();
 
-  fn from_request(request: &'a Request<'r>) -> request::Outcome<Connection, ()> {
-    let pool = request.guard::<State<Pool>>()?;
-    match pool.get() {
-      Ok(conn) => Outcome::Success(Connection(conn)),
-      Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Connection, ()> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(Connection(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+        }
     }
-  }
 }
 
 impl Deref for Connection {
-  type Target = PgConnection;
+    type Target = PgConnection;
 
-  #[inline(always)]
-  fn deref(&self) -> &Self::Target {
-    &self.0
-  }
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
