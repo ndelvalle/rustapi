478effbd36de3bda4b4e24cfb8cab612ee6ae32d
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-01-18 00:07:02 -0300

    Create error mod

--- src/errors.rs
+++ src/errors.rs
@@ -1,24 +1,59 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use serde_json::json;
+use wither::bson;
+use wither::mongodb::error::CommandError as MongoCommandError;
+use wither::mongodb::error::Error as MongoError;
+use wither::mongodb::error::ErrorKind as MongoErrorKind;
+use wither::WitherError;
 
-// use wither::WitherError;
+#[derive(thiserror::Error, Debug)]
+#[error("...")]
+pub enum Error {
+  #[error("{0}")]
+  Wither(#[from] WitherError),
 
-// #[derive(thiserror::Error, Debug)]
-// #[error("...")]
-// pub enum ApiError {
-//   // An error from the underlying `wither` library.
-//   #[error("{0}")]
-//   WitherError(#[from] WitherError),
-// }
+  #[error("{0}")]
+  Mongo(#[from] MongoError),
 
-// impl actix_web::error::ResponseError for ApiError {
-//   fn error_response(&self) -> HttpResponse {
-//     HttpResponseBuilder::new(self.status_code())
-//       .set_header(http::header::CONTENT_TYPE, "text/html; charset=utf-8")
-//       .body(self.to_string())
-//   }
+  #[error("{0}")]
+  ParseObjectID(#[from] bson::oid::Error),
 
-//   fn status_code(&self) -> http::StatusCode {
-//     match *self {
-//       ApiError::WitherError(_) => http::StatusCode::INTERNAL_SERVER_ERROR,
-//     }
-//   }
-// }
+  #[error("{0}")]
+  SerializeMongoResponse(#[from] bson::de::Error),
+}
+
+impl Error {
+  fn get_codes(&self) -> (StatusCode, u16) {
+    match *self {
+      // 4XX Errors
+      Error::ParseObjectID(_) => (StatusCode::BAD_REQUEST, 40001),
+      Error::Wither(WitherError::Mongo(MongoError { ref kind, .. })) => {
+        let mongo_error = kind.as_ref();
+        match mongo_error {
+          // MongoDB E11000 error code represent a duplicate key error
+          MongoErrorKind::CommandError(MongoCommandError { code: 11000, .. }) => {
+            (StatusCode::BAD_REQUEST, 40002)
+          },
+          _ => (StatusCode::INTERNAL_SERVER_ERROR, 5003)
+        }
+      }
+
+      // 5XX Errors
+      Error::Wither(_) => (StatusCode::INTERNAL_SERVER_ERROR, 5001),
+      Error::Mongo(_) => (StatusCode::INTERNAL_SERVER_ERROR, 5003),
+      Error::SerializeMongoResponse(_) => (StatusCode::INTERNAL_SERVER_ERROR, 5009),
+    }
+  }
+}
+
+impl IntoResponse for Error {
+  fn into_response(self) -> Response {
+    let (status_code, code) = self.get_codes();
+    let message = self.to_string();
+    let body = Json(json!({ "code": code, "message": message }));
+
+    (status_code, body).into_response()
+  }
+}
