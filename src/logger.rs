55de44fa344350887d0e94e5168dd7cad81c9566
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-05-14 20:04:20 -0300

    Update logger setup fn

--- src/logger.rs
+++ src/logger.rs
@@ -1,17 +1,15 @@
+use std::env;
+
 use crate::settings::get_settings;
 
-pub struct Logger;
+pub fn setup() {
+  if env::var_os("RUST_LOG").is_none() {
+    let settings = get_settings();
+    let level = settings.logger.level.as_str();
+    let env = format!("rustapi={},tower_http=debug", level);
 
-impl Logger {
-  pub fn setup() {
-    if std::env::var_os("RUST_LOG").is_none() {
-      let settings = get_settings();
-      let level = settings.logger.level.as_str();
-      let env = format!("rustapi={},tower_http=debug", level);
-
-      std::env::set_var("RUST_LOG", env);
-    }
-
-    tracing_subscriber::fmt::init();
+    env::set_var("RUST_LOG", env);
   }
+
+  tracing_subscriber::fmt::init();
 }
