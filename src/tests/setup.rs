5206e4788a95547ef35309ace6e390a293b842ba
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-05-29 23:24:50 -0300

    Update test setup

--- src/tests/setup.rs
+++ src/tests/setup.rs
@@ -1,27 +1,48 @@
+use bson::doc;
+use lazy_static::lazy_static;
 use std::net::SocketAddr;
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering;
 use tokio::net::TcpStream;
+use tokio::runtime::Runtime;
 use tokio::time::sleep;
 use tokio::time::Duration;
 
 use crate::create_app;
+use crate::lib::models::ModelExt;
+use crate::models::cat::Cat;
+use crate::models::user::User;
 use crate::settings::get_settings;
 
-static IS_INITIALIZED: AtomicBool = AtomicBool::new(false);
+lazy_static! {
+  static ref RUNTIME: Runtime = Runtime::new().expect("Should create a tokio runtime");
+}
 
-pub async fn setup() -> Result<(), &'static str> {
-  let is_initialized = IS_INITIALIZED.load(Ordering::Relaxed);
-  if is_initialized {
-    return Ok(());
-  }
-
+/// Use the created static tokio runtime to start the APP, wait for the server
+/// to listen on the specified port, run tests and then clean up the database.
+/// Tokio test runtime (tokio::test) is not used to avoid starting the app again
+/// on each test. Arguably this would be a better approach, but this API is
+/// statless, so there is no point on starting a new instance on each test.
+/// Make sure to run tests sequentially (cargo test -- --test-threads=1) to
+/// avoid inconsistency with the Database.
+/// Read more: https://github.com/tokio-rs/tokio/issues/2374
+pub fn with_app<F>(test: F) -> F::Output
+where
+  F: std::future::Future,
+{
   std::env::set_var("RUN_MODE", "test");
-  tokio::spawn(start_app());
-  wait_for_app_to_start().await?;
-  IS_INITIALIZED.store(true, Ordering::Relaxed);
+  RUNTIME.block_on(async move {
+    let is_app_running = is_app_running().await;
 
-  Ok(())
+    dbg!("is_app_running: {:?}", is_app_running);
+
+    if !is_app_running {
+      tokio::spawn(start_app());
+      wait_for_app_to_start().await.unwrap();
+    }
+
+    let res = test.await;
+    cleanup_database().await;
+    res
+  })
 }
 
 async fn start_app() {
@@ -37,17 +58,27 @@ async fn start_app() {
 }
 
 async fn wait_for_app_to_start() -> Result<(), &'static str> {
-  let settings = get_settings();
-  let port = settings.server.port;
-  let address = SocketAddr::from(([127, 0, 0, 1], port));
-
-  for _ in 0..5 {
-    let started = TcpStream::connect(address).await.is_ok();
-    if started {
+  for _ in 0..2000 {
+    let is_running = is_app_running().await;
+    if is_running {
       return Ok(());
     }
-    sleep(Duration::from_secs(1)).await;
+    sleep(Duration::from_millis(10)).await;
   }
 
   Err("Could not connect to APP")
+}
+
+async fn is_app_running() -> bool {
+  let settings = get_settings();
+  let port = settings.server.port;
+  let address = SocketAddr::from(([127, 0, 0, 1], port));
+  let is_running = TcpStream::connect(address).await.is_ok();
+
+  is_running
+}
+
+async fn cleanup_database() {
+  Cat::delete_many(doc! {}).await.unwrap();
+  User::delete_many(doc! {}).await.unwrap();
 }
