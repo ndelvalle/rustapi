069fe444d8187e6161d17539c2f39d95dd35c1d9
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-06-04 01:10:02 -0300

    Implement create cat test (#63)
    * Update readme
    
    * Add --color=always to tests script
    
    * Implement create cat route tests
    
    * Add --color=always to test GH action

--- src/tests/utils.rs
+++ src/tests/utils.rs
@@ -1,7 +1,9 @@
 use crate::errors::Error;
 use crate::lib::models::ModelExt;
+use crate::lib::token;
 use crate::models::user::hash_password;
 use crate::models::user::User;
+use crate::settings::get_settings;
 
 pub async fn create_user<T: AsRef<str>>(email: T) -> Result<User, Error> {
   let name = "Nahuel";
@@ -12,4 +14,12 @@ pub async fn create_user<T: AsRef<str>>(email: T) -> Result<User, Error> {
   let user = User::create(user).await?;
 
   Ok(user)
+}
+
+pub async fn create_user_token(user: User) -> Result<String, Error> {
+  let settings = get_settings();
+  let secret = settings.auth.secret.as_str();
+  let token = token::create(user, secret).unwrap();
+
+  Ok(token)
 }
