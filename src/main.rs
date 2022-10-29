5a05773b1c63a0dc4d5bd8c9ee7ca14b276f805a
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-10-29 20:15:11 +0200

    Move app creation to its own folder

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use axum::Router;
-use http::header;
 use std::net::SocketAddr;
-use tower_http::{
-  compression::CompressionLayer, propagate_header::PropagateHeaderLayer,
-  sensitive_headers::SetSensitiveHeadersLayer, trace,
-};
 use tracing::info;
 
+mod app;
 mod database;
 mod errors;
 mod lib;
@@ -28,50 +23,15 @@ use settings::get_settings;
 
 #[tokio::main]
 async fn main() {
+  let app = app::create_app().await;
+
   let settings = get_settings();
   let port = settings.server.port;
   let address = SocketAddr::from(([127, 0, 0, 1], port));
 
-  let app = create_app().await;
-
-  info!("listening on {}", &address);
+  info!("Server listening on {}", &address);
   axum::Server::bind(&address)
     .serve(app.into_make_service())
     .await
     .expect("Failed to start server");
-}
-
-pub async fn create_app() -> Router {
-  logger::setup();
-
-  database::setup()
-    .await
-    .expect("Failed to setup database connection");
-
-  models::sync_indexes()
-    .await
-    .expect("Failed to sync database indexes");
-
-  Router::new()
-    .merge(routes::status::create_route())
-    .merge(routes::user::create_route())
-    .merge(routes::cat::create_route())
-    // High level logging of requests and responses
-    .layer(
-      trace::TraceLayer::new_for_http()
-        .make_span_with(trace::DefaultMakeSpan::new().include_headers(true))
-        .on_request(trace::DefaultOnRequest::new().level(tracing::Level::INFO))
-        .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO)),
-    )
-    // Mark the `Authorization` request header as sensitive so it doesn't
-    // show in logs.
-    .layer(SetSensitiveHeadersLayer::new(std::iter::once(
-      header::AUTHORIZATION,
-    )))
-    // Compress responses
-    .layer(CompressionLayer::new())
-    // Propagate `X-Request-Id`s from requests to responses
-    .layer(PropagateHeaderLayer::new(header::HeaderName::from_static(
-      "x-request-id",
-    )))
 }
