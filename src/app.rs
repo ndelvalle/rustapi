b860e1e1da5aa7ec722aa1a69ad3a56dc48204a5
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-02-02 21:34:45 +0100

    Add CORS configuration (#198)
    * Add CORS configuration
    
    * Update comments

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use axum::Router;
 use http::header;
 use tower_http::{
-  compression::CompressionLayer, propagate_header::PropagateHeaderLayer,
+  compression::CompressionLayer, cors::CorsLayer, propagate_header::PropagateHeaderLayer,
   sensitive_headers::SetSensitiveHeadersLayer, trace,
 };
 
@@ -42,4 +42,7 @@ pub async fn create_app() -> Router {
     .layer(PropagateHeaderLayer::new(header::HeaderName::from_static(
       "x-request-id",
     )))
+    // CORS configuration. This should probably be more restrictive in
+    // production.
+    .layer(CorsLayer::permissive())
 }
