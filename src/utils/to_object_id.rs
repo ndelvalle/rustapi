0f351780b3ad9923b876513161c48e0464ffc5bf
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-02-05 22:45:00 +0100

    Update ObjectID parsing error handling (#200)
    * Update OID parsing error handling
    
    * Update error message

--- src/utils/to_object_id.rs
+++ src/utils/to_object_id.rs
@@ -3,5 +3,5 @@ use bson::oid::ObjectId;
 use crate::errors::Error;
 
 pub fn to_object_id<S: AsRef<str>>(id: S) -> Result<ObjectId, Error> {
-  ObjectId::parse_str(id.as_ref()).map_err(Error::ParseObjectID)
+  ObjectId::parse_str(id.as_ref()).map_err(|_| Error::ParseObjectID(id.as_ref().to_string()))
 }
