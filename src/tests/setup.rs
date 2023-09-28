2bf7742d80765d67a74bec37b1d8b444c88f1a25
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-09-28 12:55:06 +0200

    Remove async_once from tests

--- src/tests/setup.rs
+++ src/tests/setup.rs
@@ -1,8 +1,8 @@
-use async_once::AsyncOnce;
 use bson::doc;
 use lazy_static::lazy_static;
 use std::net::SocketAddr;
 use tokio::runtime::Runtime;
+use tokio::sync::OnceCell;
 
 use crate::app::create_app;
 use crate::models::cat::Cat;
@@ -10,25 +10,29 @@ use crate::models::user::User;
 use crate::settings::SETTINGS;
 use crate::utils::models::ModelExt;
 
-lazy_static! {
-  static ref RUNTIME: Runtime = Runtime::new().unwrap();
+static API: OnceCell<()> = OnceCell::const_new();
+
+pub async fn start_api_once() {
+  API
+    .get_or_init(|| async {
+      std::env::set_var("RUN_MODE", "test");
+
+      let app = create_app().await;
+      let port = SETTINGS.server.port;
+      let address = SocketAddr::from(([127, 0, 0, 1], port));
+
+      tokio::spawn(async move {
+        axum::Server::bind(&address)
+          .serve(app.into_make_service())
+          .await
+          .expect("Failed to start server");
+      });
+    })
+    .await;
 }
 
 lazy_static! {
-  pub static ref API: AsyncOnce<()> = AsyncOnce::new(async {
-    std::env::set_var("RUN_MODE", "test");
-
-    let app = create_app().await;
-    let port = SETTINGS.server.port;
-    let address = SocketAddr::from(([127, 0, 0, 1], port));
-
-    tokio::spawn(async move {
-      axum::Server::bind(&address)
-        .serve(app.into_make_service())
-        .await
-        .expect("Failed to start server");
-    });
-  });
+  static ref RUNTIME: Runtime = Runtime::new().unwrap();
 }
 
 pub fn use_app<F>(test: F)
@@ -36,7 +40,7 @@ where
   F: std::future::Future,
 {
   RUNTIME.block_on(async move {
-    API.get().await;
+    start_api_once().await;
 
     Cat::delete_many(doc! {}).await.unwrap();
     User::delete_many(doc! {}).await.unwrap();
